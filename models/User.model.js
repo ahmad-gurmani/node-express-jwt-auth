0d5c65b00528f3bce95280b072c65ceadbf4f461
Ahmad-Gurmani <ahmad.gurmani06@gmail.com>
2024-06-12 19:24:01 +0500

    password is hashed done before saving it in db

--- models/User.model.js
+++ models/User.model.js
@@ -1,19 +1,32 @@
 import mongoose from "mongoose";
 import validator from 'validator';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, "PLease enter an email"],
+        required: [true, "Please enter an email"],
         unique: true,
         lowercase: true,
-        validate: [validator.isEmail, "Please enter a valid email"]
+        validate: {
+            validator: (value) => validator.isEmail(value),
+            message: "Please enter a valid email"
+        }
     },
     password: {
         type: String,
         required: [true, "Please enter a password"],
         minLength: [6, "Minimum password length is 6 characters"]
     }
-})
+});
+
+// Pre and post are mongoose hooks and many more
+
+// Fire a function before saving the document
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
 
 export const User = mongoose.model('User', userSchema);
